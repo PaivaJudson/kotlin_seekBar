ddf4b62f3dde86445a6ffaaa451b5faebd61cf4b
Judson Paiva <judson.paiva@hotmail.com>
2021-09-23 03:03:11 +0100

    app finalizado com RatingBar

--- app/src/main/java/com/judson/instituto_de_cdigo_ratingbar/MainActivity.kt
+++ app/src/main/java/com/judson/instituto_de_cdigo_ratingbar/MainActivity.kt
@@ -2,10 +2,20 @@ package com.judson.instituto_de_cdigo_ratingbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rgb.setOnRatingBarChangeListener { ratingBar, rating, fromUser -> rating
+            txt_texto.text  = ""+rating
+        }
+
+        btn_executar.setOnClickListener {
+            Toast.makeText(applicationContext, "Valo selecionado = "+rgb.rating, Toast.LENGTH_LONG).show()
+        }
     }
 }
